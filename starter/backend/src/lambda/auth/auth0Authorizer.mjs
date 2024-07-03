a0164d69ac8f689de0a7a3c5e8307f20388654a0
MyoDev00 <myodev00@gmail.com>
2024-07-03 20:22:23 +0700

    add serverless offline and Auth0 fix

--- starter/backend/src/lambda/auth/auth0Authorizer.mjs
+++ starter/backend/src/lambda/auth/auth0Authorizer.mjs
@@ -4,29 +4,11 @@ import { createLogger } from '../../utils/logger.mjs'
 
 const logger = createLogger('auth')
 
-const jwksUrl = 'https://test-endpoint.auth0.com/.well-known/jwks.json'
-const certificate = `-----BEGIN CERTIFICATE-----
-MIIDHTCCAgWgAwIBAgIJanYikJOh5DqEMA0GCSqGSIb3DQEBCwUAMCwxKjAoBgNV
-BAMTIWRldi1maWtwczB5b2owOHF5OGVnLnVzLmF1dGgwLmNvbTAeFw0yNDA2MTgx
-MzQ4NThaFw0zODAyMjUxMzQ4NThaMCwxKjAoBgNVBAMTIWRldi1maWtwczB5b2ow
-OHF5OGVnLnVzLmF1dGgwLmNvbTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoC
-ggEBANk9n88pVGRG4siwkV0eUMC09d7D0otAil4GuVDeWP2+ipCPi84B6q3nDxjw
-dDOdLtyvjEi40nCMYBUeeRnAxRLoTuAHVQ//EpwiH1LCj4wHRYPKVln0OM5LCzwY
-c//sMX0x8im5Rf2ycqT2JDI4O4bt+0Z5QQgEjNOzNSOeu3G2rYmtN4CffNDL/NHl
-6ilPAND7WZthcR01SIhlzmYrIcyouV1nBpnBrudakrm1DoyOKm5zEd1bzpmvTtrW
-04/dAEhk6/HXBBOiDSZyn4HEyjDtEP9z/DxyqWxPUHUWgVmEW32TmtG53ItUrAN+
-tUs+bhc3HkApolWHxh1EF6FYJeUCAwEAAaNCMEAwDwYDVR0TAQH/BAUwAwEB/zAd
-BgNVHQ4EFgQU7WMX0ZMC4ulIbxf7dvhOrZJ5w5IwDgYDVR0PAQH/BAQDAgKEMA0G
-CSqGSIb3DQEBCwUAA4IBAQBeX2EADpM4z/WIAeJMYN7WQkiQ7MtqQ10n2LhHlDlj
-ut506AI9oZE6e/PrZbtdBZrXZ594VQhpPzwHdddTUKmZQUvXJJi+Dvix2+oRYUIH
-YN9CKswuhuSf08MKfasYWUwIV9QH78gtYh35wG2miCExyxhZE/yLL66OKCyeqdDi
-tOgSCC2OZcsgGVJtQcESnrYbKNrFJ6XcTbwO1Z/Qg+nH8bOOtFf64CoimHJgJflS
-JjKYNacDBefg60LsCtS1U32Q3FY0lJ3LPQSkMI/mwt8yiQjdcS2Hz57ynG3Um399
-VqtruJ78Ys+3o2HKS0IwSfZpTgfdopBMG9vZoegfOsNj
------END CERTIFICATE-----`
+const jwksUrl = 'https://test.auth0.com/.well-known/jwks.json'
 
 export async function handler(event) {
   try {
+    logger.info('Event token : ' + event.authorizationToken)
     const jwtToken = await verifyToken(event.authorizationToken)
 
     return {
@@ -63,7 +45,21 @@ export async function handler(event) {
 
 async function verifyToken(authHeader) {
   const token = getToken(authHeader)
-  return jsonwebtoken.verify(token, certificate, { algorithms: ['HS256'] })
+  const jwt = jsonwebtoken.decode(token, { complete: true })
+
+  const response = await Axios.get(jwksUrl)
+  const keyset = response.data.keys.find((key) => key.kid === jwt.header.kid)
+
+  logger.info('keyset', keyset)
+  if (!keyset) {
+    throw new Error('Keys is invalid')
+  }
+  const certificate = `-----BEGIN CERTIFICATE-----\n${keyset.x5c[0]}\n-----END CERTIFICATE-----\n`
+  const verifyresult = jsonwebtoken.verify(token, certificate, {
+    algorithms: ['RS256']
+  })
+  logger.info('verifyresult', verifyresult)
+  return verifyresult
 }
 
 function getToken(authHeader) {
